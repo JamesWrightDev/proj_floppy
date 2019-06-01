4d44ce623877c4ec88bc91f2f20acccc2e0d1dae
James <contact@james-wright.co.uk>
2019-06-01 21:05:32 +0100

    add mapped state,dispatch to props for release lis

--- src/components/releaseList/releaseListContainer.js
+++ src/components/releaseList/releaseListContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import ReleaseListItem from './releaseListItem';
 import axios from 'axios';
-export default class releaseListContainer extends Component {
+import { connect } from 'react-redux';
+class releaseListContainer extends Component {
     state = {
         data:[],
         loading: true
@@ -22,22 +23,33 @@ export default class releaseListContainer extends Component {
   }
 
 
-
-
     render() {
         return (
             <div>
+                <h2>{this.props.comics}</h2>
                 <h3>New Releases</h3>
+                <button onClick={this.props.addComic}>VUTTON</button>
+
                 {this.state.data.map((item,index) => (
                     <ReleaseListItem
                         title={item.title}
                         price={item.price}
-                        key={Math.random}
+                        key={Math.random()}
                         publisher={item.publisher}
+
                     />
                 ))}
-
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    const { comics } = state
+    return { comics }
+  }
+function mapDispatchToProps(dispatch){
+    return{
+        addComic: () => dispatch({ type: 'ADD_COMIC', payload:'gello' } )
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(releaseListContainer)
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,10 @@ import { identifier } from "@babel/types";
 import { ADD_COMIC } from "../constants/action-types";
 
 const initialState = {
-    comics: []
+    comics: [],
   };
   function rootReducer(state = initialState, action) {
+
     if(action.type === ADD_COMIC){
       return Object.assign({}, state, {
         comics: state.comics.concat(action.payload)
